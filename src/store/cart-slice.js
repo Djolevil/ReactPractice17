6b428f01b512fa46e99e9c9753638cedf6b929f1
Djordje Knezevic <starwarrior@mycity.rs>
2022-06-16 19:38:04 +0200

    Fix typo in cart-slice.js

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -30,7 +30,7 @@ const cartSlice = createSlice({
         const id = action.payload;
         const existingItem = state.items.find(item => item.id === id);
         state.totalQuantity--;
-        tate.changed = true;
+        state.changed = true;
         if (existingItem.quantity === 1) {
             state.items = state.items.filter(item => item.id !== id);
         } else {
